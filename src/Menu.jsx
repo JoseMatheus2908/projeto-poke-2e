1d5eafe6bae24dde1db8af48ed73c2bb31551727
JoseMatheus2908 <rm95206@estudante.fieb.edu.br>
2025-08-25 14:04:00 +0000

    Menu

---
+++ src/Menu.jsx
@@ -0,0 +1,24 @@
+const Menu=() => {
+    return (
+        <div>
+            <ul className="nav">
+                <li className="nav-item">
+                    <a class="nav-link" href="#">
+                        Active
+                    </a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="/home">
+                    Home
+                    </a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="/viacep">
+                    ViaCep
+                    </a>
+                </li>
+            </ul>
+        </div>
+    )
+}
+export default Menu
